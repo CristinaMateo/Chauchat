284472a8c7ce6d3e0123e5a972e396bb8650b273
Cristina Mateo <cris.mateo.99@gmail.com>
2023-09-26 18:02:42 +0200

    FUNCIONA

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let cargador = "pium!"
 
 function chauchat(){
-    let num = parseInt( prompt("Introduzca un número")); 
+ let num = parseInt( prompt("Introduzca un número")); 
+    let resultado = cargador.repeat(num).match(/.{1,15}(.$)?/g)
     if(typeof num == "number"){
-        return cargador.repeat(num)
-    } 
-    return "Illo, me he quedao pillá!"
+        return resultado.join(" ")
+    }
+        return "Illo, me he quedao pillá!"
 }
 
-console.log(chauchat("ramita"))
+console.log(chauchat())
 
-
- 
+ //cada 14 caracteres se debería dividir
