9cfb77db945b0d638ad4fd340c88da3429c47366
Cristina Mateo <cris.mateo.99@gmail.com>
2023-09-26 18:41:49 +0200

    ara sí

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 let cargador = "pium!"
-
+/*
 function chauchat(){
  let num = parseInt( prompt("Introduzca un número")); 
     let resultado = cargador.repeat(num).match(/.{1,15}(.$)?/g)
     if(typeof num == "number"){
-        return resultado.join(" ")
+        return resultado.join("\n")
     }
         return "Illo, me he quedao pillá!"
 }
 
 console.log(chauchat())
 
- //cada 14 caracteres se debería dividir
+ */
+
+ // 
+ function chauchat(){
+    let num = parseInt( prompt("Introduzca un número "));
+    if(isNaN(num)){
+        return "Illo, me he quedao pillá!"
+    }
+    let resultado = ""
+    for(let i=1; i<=num; i++ ){
+        resultado = resultado.concat(cargador, "\n")
+        if(i % 3 === 0){
+           resultado = resultado.concat("\n")
+        }
+    }
+    return resultado
+}
+
+console.log(chauchat())
